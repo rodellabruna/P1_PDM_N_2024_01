e97ba81ea99094d892693551f947c51c72b08617
WanessaW <vasodasilva@gmail.com>
2024-04-18 22:31:18 -0300

    feat(p1-pdmn):exibe lista real

--- src/LembreteNovo.js
+++ src/LembreteNovo.js
@@ -1,35 +1,41 @@
 import React, { Component } from 'react';
 import LembreteEntrada from './LembreteEntrada';
- 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+
 export default class LembreteNovo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            textos: [
-                "Preparar Aula de Programação",
-                "Fazer feira",
-                "Preparar marmitas"
-            ]
+            textos: []
         };
     }
- 
-    atualizarTexto = (novoTexto, index) => {
+
+    atualizarTexto = (novoTexto) => {
+        this.setState({ textos: this.state.textos.concat(novoTexto) });
+    }
+
+    removerLembrete = (index) => {
         const newTextos = [...this.state.textos];
-        newTextos[index] = novoTexto;
+        newTextos.splice(index, 1);
         this.setState({ textos: newTextos });
     }
- 
+
     render() {
         return (
             <div className='container'>
-                <div className='card' style={{ backgroundColor: 'lightblue', minHeight: '40vh', padding: '20px' }}>
+                <div className='card' style={{ backgroundColor: 'lightblue', minHeight: '10vh', padding: '20px' }}>
+                    <h2 className="d-flex align-items-center justify-content-center" style={{ color: "rgb(14, 120, 172)" }}>Lista de Lembretes</h2>
                     {this.state.textos.map((texto, index) => (
                         <div key={index} style={{ width: '80%', margin: '0 auto' }}>
                             <div className="d-flex align-items-center justify-content-center">
                                 <div className="col-md-8 col-12">
-                                    <div className="card-body" style={{borderRadius: '25px', borderColor: 'black'}}>
-                                        <div className="d-flex align-items-center justify-content-center border rounded mb-1" style={{ height: '5rem', backgroundColor: 'rgb(14, 120, 172)', color: 'white'  }}>
+                                    <div className="card-body" style={{ borderRadius: '25px', borderColor: 'black' }}>
+                                        <div className="d-flex align-items-center justify-content-between border rounded" style={{ height: '5rem', backgroundColor: 'rgb(14, 120, 172)', color: 'white', padding: '10px', fontSize: '20px' }}>
                                             {texto}
+                                            <button onClick={() => this.removerLembrete(index)} style={{background: 'transparent' , border: "none", cursor: "pointer", outline: "none", padding: "0"}}>
+                                                <FontAwesomeIcon icon={faTrashCan} style={{color:"white", fontSize: '20px'}} />
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
@@ -40,10 +46,7 @@ export default class LembreteNovo extends Component {
                 </div>
                 <br></br>
                 <LembreteEntrada atualizarTexto={this.atualizarTexto} />
-            </div>        
+            </div>
         );
     }
 }
-
-
- 
--- src/index.js
+++ src/index.js
@@ -1,39 +1,32 @@
-import React from 'react'; 
-import ReactDOM from 'react-dom'; 
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
-import LembreteNovo from './LembreteNovo'; 
-import LembreteLista from './LembreteLista';
-import LembreteEntrada from './LembreteEntrada'; 
+import React from 'react';  
+import ReactDOM from 'react-dom';  
+import 'bootstrap/dist/css/bootstrap.min.css';  
 
- 
+//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";  
+import LembreteNovo from './LembreteNovo';  
 
-class App extends React.Component { 
+class App extends React.Component {  
 
-  render() { 
-    return ( 
-      <div className='container'> 
+  render() {  
+    return (  
+      <div className='container'>  
+        <br></br>  
+        <div className="row justify-content-center">  
+            <div className="col-sm-12 col-md-8">  
+                <LembreteNovo />  
+            </div>  
+        </div>  
+      </div>  
+    );  
 
-        <br></br> 
-
-        <div className="row justify-content-center"> 
-            <div className="col-sm-12 col-md-8"> 
-                <LembreteNovo /> 
-            </div> 
-        </div> 
-      </div> 
-
-    ); 
-
-  } 
-
-} 
- 
-
-ReactDOM.render( 
-  <App />, 
-  document.querySelector('#root') 
+  }  
+}  
+ReactDOM.render(  
+  <App />,  
+  document.querySelector('#root')  
 
 ); 
+
+ 
 
  
